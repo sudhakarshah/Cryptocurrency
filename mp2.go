039ff010964a491dd1141100c8a58256fd7df3ce
Kim <kkim103@sp19-cs425-g62-01.cs.illinois.edu>
2019-03-26 00:15:10 -0500

    stuff

--- mp2.go
+++ mp2.go
@@ -215,16 +215,15 @@ func main(){
 			if _, ok := members[target_id];ok{
 				continue
 			}
-			init := FormatInit(members, hashtable, len(members))
+			ping := FormatPing(members)
 			nd := Node{Name:m.GetName(), Ip:m.GetIp(), Port:m.GetPort(), LastActive:int64(time.Now().Unix()), Sock:m.Sock, Attempts:0}
 			go nd.ListenToFriend(&inbox)
 			members[fmt.Sprintf("%s:%s:%s",m.GetName(),m.GetIp(),m.GetPort())] = &nd
-			for _, msg := range init{
-				if msg.GetType() == "INTRODUCE"{
-					if rand.Intn(3) == 0{
-						nd.SendJson(msg)
-					}
+			for i, msg := range ping{
+				if i > 4{
+					break
 				}
+				nd.SendJson(msg)
 			}
 
 
